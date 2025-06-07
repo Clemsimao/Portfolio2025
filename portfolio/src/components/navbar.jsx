6e2dd4f62066073a17991bd99673c5e5648ed99a
Clemsimao <simaoclement33@gmail.com>
2025-06-07 15:47:38 +0200

    add: work page done

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -37,16 +37,14 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
 
                 <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
                     <li><a className='font-Ovo' href="#top">Accueil</a></li>
-                    <li><a className='font-Ovo' href="#about">Moi</a></li>
-                    <li><a className='font-Ovo' href="#services">Services</a></li>
+                    <li><a className='font-Ovo' href="#about">Moi</a></li> 
                     <li><a className='font-Ovo' href="#work">Mes projets</a></li>
-                    <li><a className='font-Ovo' href="#contact">Contactez moi</a></li>
                 </ul>
 
                 <div className='flex items-center gap-4'>
-                    <button onClick={() => setIsDarkMode(prev => !prev)}>
+                    {/*<button onClick={() => setIsDarkMode(prev => !prev)}>
                         <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
-                    </button>
+                    </button>*/}
 
                     <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>
                         Contact
@@ -60,7 +58,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
             </nav>
 
             {/* Menu pour les petits écrans */}
-            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-orange-50 transition duration-500 dark:bg-darkHover dark:text-white'>
                 <div className='absolute right-6 top-6' onClick={closeMenu}>
                     <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer' />
                 </div>
---
+++ portfolio/src/components/work.jsx
@@ -0,0 +1,68 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Work = ({isDarkMode}) => {
+  return (
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1 }}
+    id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+
+      <motion.h4 
+      initial={{ y: -20, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0.3, duration: 0.5 }}
+      className='text-center mb-2 text-lg font-Ovo'>
+      Mon portfolio</motion.h4>
+
+      <motion.h2
+      initial={{ y: -20, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0.5, duration: 0.5 }}
+      className='text-center text-5xl font-Ovo'>
+      Mes dernières réalisations</motion.h2>
+
+      <motion.p
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ delay: 0.7, duration: 0.5 }}
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+      Bienvenue sur mon portfolio !
+    Vous y découvrirez une sélection de projets mettant en valeur mes compétences, réalisés dans le cadre de ma formation et de mes projets personnels.</motion.p>
+
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ delay: 0.9, duration: 0.6 }}
+    className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-5 dark:text-black'>
+        {workData.map((project, index)=>(
+            <motion.div
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
+            key={index}
+            className='aspect-video bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+            style={{ backgroundImage: `url(${project.bgImage.src})` }}>
+                <div className='bg-[#F0AD4E] w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                    <div>
+                        <h2 className='font-semibold'>{project.title}</h2>
+                        <p className='text-sm text-gray-700'>{project.description}</p>
+                    </div>
+                    <div className='bg-white border rounded-full border-black w-9 aspect-video flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-orange-300 transition'>
+                        <Image src={assets.send_icon} alt='send icon' className='w-5'/>
+                    </div>
+                </div>
+                
+            </motion.div>
+        ))}
+    </motion.div>
+
+    
+
+    </motion.div>
+  )
+}
+
+export default Work
